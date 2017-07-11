49a8aca6383e6e210580c3a2314f9f99843984ed
Matt Fontanilla <mattfontanilla26@gmail.com>
2017-07-11 13:15:30 -0500

    Add files via upload

---
+++ CountDGroupMembers.ps1
@@ -0,0 +1,11 @@
+﻿$GroupName = Get-Content ".\export.txt"
+
+foreach($Group in $GroupName){
+
+$count = (Get-ADGroupMember -Identity $($Group)).count
+
+Write-Host "$Group has $count members"
+
+}
+
+Read-Host -Prompt "Press Enter to continue"
